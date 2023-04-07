051dcb65967642e12ce20f7f6dd056197271b08a
Nirnay Behera <nirnay.1behera@gmail.com>
2023-04-07 16:43:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import {Routes, Route} from 'react-router-dom';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Detail from './pages/Detail';
+import AddEditTask from './pages/AddEditTask';
+import About from './pages/About';
+import Notfound from './pages/Notfound';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/detail/:id" element={<Detail/>} />
+        <Route path="/add/:id" element={<AddEditTask/>} />
+        <Route path="/update/:id" element={<AddEditTask/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="*" element={<Notfound/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/AddEditTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddEditTask = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddEditTask
